f8ba34d8a2c431d1b35e1ff042125212b33aa857
Enzo Colluto <vcolluto>
2022-10-26 22:59:23 +0200

    Aggiunto CrossOrigin

--- Academy/src/main/java/com/corsojava/academy/controller/CourseController.java
+++ Academy/src/main/java/com/corsojava/academy/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.corsojava.academy.model.Course;
 import com.corsojava.academy.repository.CourseRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")		//tutte le richieste del tipo /api/*
 public class CourseController {
--- Academy/src/main/java/com/corsojava/academy/controller/StudentController.java
+++ Academy/src/main/java/com/corsojava/academy/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.corsojava.academy.model.Student;
 import com.corsojava.academy.repository.StudentRepository;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api")		//tutte le richieste del tipo /api/*
 public class StudentController {
